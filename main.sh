7f53da3e1637506751fc754c0cd7c7dcd8e40149
Nataliya_Khanova <natalia.khanum@gmail.com>
2024-09-09 19:37:47 +0300

    addmain

---
+++ main.sh
@@ -0,0 +1,5 @@
+You switch back and forth between branches using git checkout <branch name>.
+
+And yes, git checkout -b NEW_BRANCH_NAME is the correct way to create a new branch and switching to it. 
+At the same time, the command you used is a shorthand to git branch <branch name> 
+and git checkout <branch name>.
